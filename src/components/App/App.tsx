cec9a2b2e5430784ebc9235ae933c965c128077b
Artem <stuzukartem12@gmail.com>
2026-03-20 13:48:02 +0200

    jsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import css from "./App.module.css";
 import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import VoteOptions from "../VoteOptions/VoteOptions.tsx";
-import VoteStats from "../VoteStatus/VoteStats.js";
+import VoteStats from "../VoteStats/VoteStats.js";
 import Notification from "../Notification/Notification.tsx";
 import { type Votes, type VoteType } from "../../types/votes.ts";
 export default function App() {
